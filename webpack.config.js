159aa43e9f9aabcd0fc672e333570b13a6e9c740
tianbingyao22 <2242378609@qq.com>
2022-06-27 20:43:15 +0800

    添加自动生成html文件等插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
-const path = require("path")
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 // 修改入口出口
 module.exports = {
+    mode:'development',
     entry: "./src/main.js", // 入口
     output: { 
         path: path.resolve(__dirname, "lib"), // 出口路径 绝对路径
         filename: "index.js" // 出口文件名
-    }
+    },
+    // 插件
+    plugins:[
+        // 自动生成html文件
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',//参考模板
+            filename:'index.html',
+        }),
+        // 自动清除出口目录内容
+        new CleanWebpackPlugin(),
+    ]
 }
